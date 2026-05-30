921a9ef3ae48694f1ce4b207bd79299ddfc96646
fatimajber881-source <fatimajber881@gmail.com>
2026-05-30 22:19:58 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,3 +5,9 @@ const pool = new Pool({
   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   ssl: { rejectUnauthorized: false },
 });
+
+pool.connect()
+  .then(() => console.log('✅ PostgreSQL Connected'))
+  .catch(err => console.error('❌ DB Error:', err.message));
+
+module.exports = pool;
